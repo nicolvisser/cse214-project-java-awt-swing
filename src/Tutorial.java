54e0a56280ee58213fb3ede86c7ad327540aa5f0
Nicol Visser <nicolvisser@yahoo.com>
2020-05-11 14:18:24 +0200

    include use of shield in tutorial

--- src/Tutorial.java
+++ src/Tutorial.java
@@ -27,10 +27,16 @@ public class Tutorial extends DisplayComponent {
 
     int tutorialStage = 0;
 
-    boolean objective1Complete, objective2Complete, objective3Complete;
+    boolean hasAimTarget, objective1Complete, objective2Complete, objective3Complete;
 
     Rectangle moveTarget = new Rectangle(vw / 4, shooter.position.y, shooter.width * 2, shooter.height * 2);
 
+    /**
+     * Simple tutorial with objectives to get to know controls of game
+     * 
+     * @param keyCodes        key codes for player 1 controls
+     * @param keyDescriptions desriptions of keys associated with player 1 controls
+     */
     public Tutorial(int[] keyCodes, String[] keyDescriptions) {
         this.keyCodes = keyCodes;
         this.keyDescriptions = keyDescriptions;
@@ -48,10 +54,21 @@ public class Tutorial extends DisplayComponent {
         for (Enemy enemy : enemies)
             enemy.update(dt);
 
+        // Objective 1: Move into a box
         if (!objective1Complete)
             objective1Complete = moveTarget.contains(shooter.getCollisionShape());
-        objective2Complete = Double.isFinite(getLengthOfAImLine());
-        objective3Complete = enemies.size() <= 0;
+
+        // Objective 2: Aim at enemy
+        hasAimTarget = Double.isFinite(getLengthOfAImLine());
+
+        // Objective 3: Kill all enemies
+        objective2Complete = enemies.size() <= 0;
+
+        // Objective 4: Demonstrate the use of your shield
+        shooter.energyPoints = 100; // ensure shooter always has enough energy to use shield
+        System.out.println(shooter.isShieldActive);
+        if (shooter.isShieldActive)
+            objective3Complete = true;
 
         Collidable.checkAndHandleCollisions(shooter.missiles, enemies);
         Disposable.handleDisposing(shooter.missiles);
@@ -81,7 +98,11 @@ public class Tutorial extends DisplayComponent {
         g2.setColor(Color.gray);
         Utils.drawCenteredText(g2, vw / 2, vh * 25 / 100, "You can change the controls under Settings in Main Menu", 1);
 
-        if (!objective3Complete) {
+        if (objective1Complete && objective2Complete && objective3Complete) {
+            g2.setColor(Color.GREEN);
+            Utils.drawCenteredText(g2, vw / 2, vh * 20 / 100,
+                    "You now know the basics. Press escape and start a new game.", 2);
+        } else {
             if (!objective1Complete) {
                 g2.setColor(Color.RED);
                 Utils.drawCenteredText(g2, vw / 2, vh * 20 / 100,
@@ -89,11 +110,11 @@ public class Tutorial extends DisplayComponent {
                 drawKey(g2, vw * 45 / 100, vh * 30 / 100, keyDescriptions[0]);
                 drawKey(g2, vw * 55 / 100, vh * 30 / 100, keyDescriptions[1]);
                 moveTarget.draw(g2);
-            } else {
+            } else if (!objective2Complete) {
                 for (Enemy enemy : enemies)
                     enemy.draw(g2);
 
-                if (!objective2Complete) {
+                if (!hasAimTarget) {
                     g2.setColor(Color.ORANGE);
                     Utils.drawCenteredText(g2, vw / 2, vh * 20 / 100, "Aim at the enemy using the aim keys", 2);
                     drawKey(g2, vw * 45 / 100, vh * 30 / 100, keyDescriptions[2]);
@@ -103,11 +124,12 @@ public class Tutorial extends DisplayComponent {
                     Utils.drawCenteredText(g2, vw / 2, vh * 20 / 100, "Shoot at the enemy with the shoot key", 2);
                     drawKey(g2, vw * 50 / 100, vh * 30 / 100, keyDescriptions[4]);
                 }
+            } else {
+                g2.setColor(Color.CYAN);
+                Utils.drawCenteredText(g2, vw / 2, vh * 20 / 100,
+                        "You can use your shield to block enemy attacks. Try it now.", 2);
+                drawKey(g2, vw * 50 / 100, vh * 30 / 100, keyDescriptions[5]);
             }
-        } else {
-            g2.setColor(Color.GREEN);
-            Utils.drawCenteredText(g2, vw / 2, vh * 20 / 100,
-                    "You now know the basics. Press escape and start a new game.", 2);
         }
 
         drawAimLine(g2);
@@ -132,7 +154,7 @@ public class Tutorial extends DisplayComponent {
             } else if (e.getKeyCode() == keyCodes[4]) { // Shoot
                 shooter.onShootButtonPress();
             } else if (e.getKeyCode() == keyCodes[5]) { // Block
-
+                shooter.activateShield();
             }
         }
 
@@ -149,7 +171,7 @@ public class Tutorial extends DisplayComponent {
             } else if (e.getKeyCode() == keyCodes[4]) { // Shoot
                 shooter.onShootButtonRelease();
             } else if (e.getKeyCode() == keyCodes[5]) { // Block
-
+                shooter.deactivateShield();
             }
         }
 
